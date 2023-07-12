c203faf2d42169faed84f38f0f0cc20960dda747
BrunoBuona <brunobuonassisa@gmail.com>
2023-07-12 05:49:28 -0300

    block 2 of 9

--- src/pages/Home/Block2/Block2.jsx
+++ src/pages/Home/Block2/Block2.jsx
@@ -6,13 +6,39 @@ export default function Block2() {
         <div className="block2-block">
           <main className='main-block2'>
           <div className='col1-block2'>
-          <h1>Columna 1 - Bloque 2</h1>
+          <h2 style={{textAlign:'left'}}>
+          ÚLTIMO 
+          <br />
+          LANZAMIENTO!
+          </h2>
+          <p>Mira el adelanto sin censura aquí 👇🏻</p>
+          <iframe src="https://www.xvideos.com/embedframe/77288537" frameborder="0" width="350" height="200" style={{border:'3px solid black'}} scrolling="no" allowfullscreen="allowfullscreen"></iframe>
           </div>
           <div className='col2-block2'>
-          <h1>Columna 2 - Bloque 2</h1>
+          <h2>The BAD Dentist 🦷</h2>
+          <h3>Una odontóloga muy poco profesional</h3>
+          <p>En esta historia, Claudia, pierde su turno al dentista, y le pide a su asistente personal, una App que se llama (<span className='underline'>Sof</span>tware + <span className='underline'>IA</span>) Pero la inteligencia artificial falla, y le recomienda ir con la Dr. Delfina Palacios. Es el año 2030, graduarse sigue siendo muy importante, pero Delfina, una estudiante novata de odontología, no lo ve así. Claudia acude a sus servicios por un dolor, pero además de ser atendida en el domicilio particular de Delfina, la doctora tiene unos métodos odontológicos un tanto… particulares. Increíblemente logra curar a su paciente en tan solo unos momentos, pero esto genera algunos ruidos que alertan al Dr. Ramirez que estaba trabajando en la planta superior. Episodio que concluye en una atención «personalizada» doble!</p>
           </div>
           <div className='col3-block2'>
-          <h1>Columna 3 - Bloque 2</h1>
+            <h2>Ficha Tecnica:</h2>
+              <p>
+                ✅ Calidad: Full-HD | 60fps | 50bits/s 
+              <br/>
+                🎬 Rodaje: Cámara Profesional
+              <br/>
+                ⏳ Duración: 22 minutos
+              <br/>
+                🌀 Escena: Lésbico + Trío MHM
+              <br/>
+                📅 Lanzamiento: 2023
+              <br/>
+            </p>
+            <h2>Comprar</h2>
+            <p style={{width:'350px'}}>⚠️ Una vez abonado, enviame el comprobante de pago haciendo <a style={{color:'#dc2254',textDecoration:'none'}} href="https://t.me/cielitobebe">click acá.</a></p>
+            <div className='block-btns-block3'>
+            <button>Adquirir | Mercado Pago $5000 </button>
+            <button>Adquirir | PayPal $20 USD </button>
+            </div>
           </div>
           </main>
         </div>
